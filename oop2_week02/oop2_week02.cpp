b88abd419f942710842a47634b0743ca62cd096e
inhawinter <inhawinter@gmail.com>
2023-06-20 10:53:58 +0900

    week02 prime number v0.3

--- oop2_week02/oop2_week02.cpp
+++ oop2_week02/oop2_week02.cpp
@@ -2,19 +2,22 @@
 
 int main() {
 	int number = 0;
-	int count = 0;
+	//int count = 0;
+	bool isPrime = true;  // less memory, readability
 
 	std::cout << "Input number : ";
 	std::cin >> number;
 
 	for (int i = 2; i < number; i++) {
 		if (number % i == 0) {
-			count++; //count = count + 1;
+			//count++; //count = count + 1;
+			isPrime = false; // remove add operation
 		}
 	}
 
 
-	if (count == 0)
+	//if (count == 0)
+	if (isPrime != 0)
 		std::cout << number << " is prime number~\n";
 	else
 		std::cout << number << " is NOT prime number!\n";
