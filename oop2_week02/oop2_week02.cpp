2e13b4b624b919ff9e5ecedf133d69d8be72251e
inhawinter <inhawinter@gmail.com>
2023-06-21 09:47:13 +0900

    week03 reference v0.1

--- oop2_week02/oop2_week02.cpp
+++ oop2_week02/oop2_week02.cpp
@@ -1,14 +1,12 @@
 #include <iostream>
 
-bool isPrime(int n) { // pass by value
+bool isPrime(int n) {
 	if (n < 2) {
 		return false;
 	}
 	else {
 		for (int i = 2; i < n; i++) {
 			if (n % i == 0) {
-				//isPrime = false;
-				//break;
 				return false;
 			}
 		}
