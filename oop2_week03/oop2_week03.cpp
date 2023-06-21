32fee2c036c111cce4a9211f29df71033b4946fa
inhawinter <inhawinter@gmail.com>
2023-06-21 09:59:08 +0900

    week03 reference v0.3

--- oop2_week03/oop2_week03.cpp
+++ oop2_week03/oop2_week03.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 using namespace std;
 
-void test(int ra) {  // call by value
+void test(int& ra) {  // call by reference
 	ra = 7;
+	cout << &ra << '\n';
 }
 
 int main() {
 	int a = 1;
+	cout << &a << '\n';
 	cout << a << '\n';
 	test(a);
 	cout << a << '\n';
